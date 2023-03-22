b933f405ee8d9a4d0132e5fa45ce9b80ec566793
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-03-22 13:14:47 -0500

    validateUserId, validateUser, validatePost are all finished.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timeStamp = new Date().toLocaleString();
@@ -7,16 +9,62 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try{
+    const user = await User.getById(req.params.id);
+    if(!user){
+      res.status(404).json({
+        message: 'user not found'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  }catch(err){
+    //comeback to this
+    res.status(500).json({
+      message: 'unnable to find user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  try{
+    const { name } = req.body;
+    if(!name || !name.trim()) {
+      res.status(400).json({
+        message: "missing required name field"
+      })
+    } else {
+      req.name = name.trim()
+      next()
+    }
+  }catch(err){
+    res.status(500).json({
+      message: 'unnable to find user'
+  })
+}
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body
+  try{
+    if(!text || !text.trim()) {
+      res.status(400).json({
+        message: "missing required text field"
+      })
+    } else {
+      req.text = text.trim()
+      next()
+    }
+  }catch(err){
+    res.status(500).json({
+      message: 'unnable to find post'
+    })
+  }
 }
 
 // do not forget to expose these functions to other modules
